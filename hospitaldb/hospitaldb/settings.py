2d11bc477fb74687a6bf7a198ec68d8b02f8c2b5
Your Name <61065698+zbiernat2000@users.noreply.github.com>
2021-11-30 22:47:49 -0500

    swap db to sql

--- hospitaldb/hospitaldb/settings.py
+++ hospitaldb/hospitaldb/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'hospitaldb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hospitaldb',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
