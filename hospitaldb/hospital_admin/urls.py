cc9ad5891f53d5e4f5ae4d9a0a97aa8c89ec3ee0
Zachary Biernat <61065698+zbiernat2000@users.noreply.github.com>
2021-12-02 13:46:01 -0500

    make first template

--- hospitaldb/hospital_admin/urls.py
+++ hospitaldb/hospital_admin/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^doctors/', views.doctors, name="index"),
+    path('docotors/', views.doctors, name='doctors')
 ]
--- hospitaldb/hospital_admin/views.py
+++ hospitaldb/hospital_admin/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+from .models import Doctor
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+#TODO do table to display doctor
+def doctors(request):
+    doctorlist = Doctor.objects.all()
+    context = {'doctorlist': doctorlist}
+    return render(request, 'Doctors.html', context)
