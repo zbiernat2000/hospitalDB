78131cf3594fee319fee8b783d9bfaecdb9dc98a
April McBroom <missmcone5@outlook.com>
2021-12-03 12:24:36 -0500

    Create forms.py
    Creation of Form.py Needs debugging

---
+++ hospitaldb/hospital_admin/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+from hospital_admin.models import Department, Doctor, Patient, Appointment, Procedure, ProcudreOrder
+
+class DepartmentForm(forms.ModelForm):
+    class Meta:
+        model = Department
+        fields = '__all__'
+class DoctorForm(forms.ModelForm):
+    class Meta:
+        model = Doctor
+        fields = '__all__'
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = '__all__'
+class AppointmentForm(forms.ModelForm):
+    class Meta:
+        model = Appointment
+        fields = '__all__'
+class ProcedureForm(forms.ModelForm):
+    class Meta:
+        model = Procedure
+        fields = '__all__'
+class ProcedureOrderForm(forms.ModelForm):
+    class Meta:
+        model = ProcudreOrder
+        fields = '__all__'
+
+
+
