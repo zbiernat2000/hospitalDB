f487b600f2962e60e1cfd2668bfb8292960ae93c
Your Name <61065698+zbiernat2000@users.noreply.github.com>
2021-12-03 23:50:21 -0500

    update and delete doctor

--- hospitaldb/hospital_admin/forms.py
+++ hospitaldb/hospital_admin/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
 from .models import Doctor
+from .models import Department
+
+
+
 
 class DoctorForm(forms.ModelForm):
+   # deparment = forms.SlugRelatedField(slug_field="departmentName", queryset= Department.objects.all())
+
     class Meta:
         model = Doctor
         fields = ('departmentID', 'first_name', 'last_name', 'email', 'phone_number', 'pager_number', 'salary')
