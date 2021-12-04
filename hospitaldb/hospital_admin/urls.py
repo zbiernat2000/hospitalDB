79e0fa3b71d005c1518d90c8bf2a203f829df40a
Zachary Biernat <61065698+zbiernat2000@users.noreply.github.com>
2021-12-04 13:22:20 -0500

    Add buttons to go back and forth between index and doctors list

--- hospitaldb/hospital_admin/urls.py
+++ hospitaldb/hospital_admin/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'^doctors/', views.doctors, name="index"),
+    #url(r'^doctors/', views.doctors, name="index"),
     path('doctor_create/', views.doctor_form, name='doctor_create'),
     path('doctor_update/<int:id>/', views.doctor_form, name='doctor_update'),
     path('doctors/', views.doctors, name='doctors'),
